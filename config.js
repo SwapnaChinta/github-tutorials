bd5ae479febcd61012e2004dc4439f81002cafa0
swapna.chinta <swapna.chinta@avanade.com>
2024-06-24 14:49:48 +1000

    924822d317b895f842751dd5448355795cb6944a

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 console.log("change to new branch again");
 console.log('watching supersimple dev and uploading changes to github accordingly');
 console.log('testing if the sync works from toolbar');
+console.log("commiting changes to github using terminal");
